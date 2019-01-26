e683d82e05776339269ce986d99c3be3cf263bba
xiajiayu <42640648+xiajiayu@users.noreply.github.com>
2019-01-26 14:38:07 +0800

    Add files via upload

--- BallCenter.cpp
+++ BallCenter.cpp
@@ -1,206 +1,160 @@
-#include<opencv2/opencv.hpp>
+#include "opencv2/highgui.hpp"
+#include "opencv2/highgui.hpp"
+
+#include "opencv2/imgproc.hpp"
+
 #include<iostream>
+#include"BallCenter.h"
+
 
 using namespace std;
+
 using namespace cv;
-int lowH =155, highH = 185, lowS = 34, highS = 255, lowV = 42, highV = 255;
-void segmentImg(int,void*);
-void FindContours(Mat&Image,Mat&ContourImage);
-Mat src;
-Mat RSrc;
-Mat dst;
-vector<vector<Point>> contours;
-vector<Vec4i>hierarcy;
-int main() {
-	VideoCapture cap(0);
-	while (cap.isOpened()) {
-		cap >> src;
-	//src = imread("src3.png");
-		//Mat splited[3];
-		//split(src, splited);
-		//imshow("a",splited[0]);
-		RSrc = Mat::zeros(src.size(), src.type());
+
+
+BallCenter::BallCenter() {};
+
+
+ void BallCenter::process(Mat&src) {
+		//src = imread("C://å›¾ç‰‡1.png");
+
+			//Mat splited[3];
+
+			//split(src, splited);
+
+			//imshow("a",splited[0]);
+		//Mat RSrc;
+		Mat dst=Mat::zeros(src.size(),src.type());
+		//RSrc = Mat::zeros(src.size(), src.type());
+
 		Mat ContourImg = src.clone();
-		//GaussianBlur(src,RSrc, Size(5, 5),0.0);
-		namedWindow("Hsv", 0);
-		createTrackbar("LH", "Hsv", &lowH, 255,segmentImg);
+		/*namedWindow("Hsv", 0);
+		createTrackbar("LH", "Hsv", &lowH, 255, segmentImg);
 		createTrackbar("HH", "Hsv", &highH, 255, segmentImg);
 		createTrackbar("LS", "Hsv", &lowS, 255, segmentImg);
-		createTrackbar("HS", "Hsv", &highS, 255,segmentImg);
+		createTrackbar("HS", "Hsv", &highS, 255, segmentImg);
 		createTrackbar("LV", "Hsv", &lowV, 255, segmentImg);
-		createTrackbar("HV", "Hsv", &highV, 255, segmentImg);
-		segmentImg(0,0);
-		FindContours(dst, ContourImg);
-		imshow("Contours", ContourImg);
-		waitKey(1);
-	}
-	return 0;
-}
-//Í¼Ïñ¶şÖµ»¯
-//Ostu·¨
-/*int Ostu(Mat &image) {
-	int width = image.cols;
-	int height = image.rows;
-	int x = 0, y = 0;
-	int pixelCount[256];
-	float pixelPro[256];
-	int i, j, pixelSum = width * height, threshold = 0;
+		createTrackbar("HV", "Hsv", &highV, 255, segmentImg);*/
 
-	uchar* data = (uchar*)image.data;
+		segmentImg(src,dst);
+        vector<vector<Point> > contour;
+        vector<Point2f> point_img;
+         contours_filter(dst, contour);
+         point_img = pick_point(contour);
+         //visual_point(src,point_img);
+     Point2f src_vertices[4];
+     src_vertices[0] = point_img[0];
+     src_vertices[1] = point_img[1];
+     src_vertices[2] = point_img[2];
+     src_vertices[3] = point_img[3];
+     //é€è§†å˜æ¢
+     Mat warpMatrix = getPerspectiveTransform(src_vertices, dst_vertices);
+     Mat warped;
+     warpPerspective(dst, warped, warpMatrix, warped.size(), INTER_LINEAR, BORDER_CONSTANT);
+     imshow("warped",warped);
+     FindContours(warped, ContourImg);
 
-	//³õÊ¼»¯  
-	for (i = 0; i < 256; i++)
-	{
-		pixelCount[i] = 0;
-		pixelPro[i] = 0;
-	}
+     namedWindow("Contours", 1);
 
-	//Í³¼Æ»Ò¶È¼¶ÖĞÃ¿¸öÏñËØÔÚÕû·ùÍ¼ÏñÖĞµÄ¸öÊı  
-	for (i = y; i < height; i++)
-	{
-		for (j = x; j < width; j++)
-		{
-			pixelCount[data[i * image.step + j]]++;
-		}
-	}
-
-
-	//¼ÆËãÃ¿¸öÏñËØÔÚÕû·ùÍ¼ÏñÖĞµÄ±ÈÀı  
-	for (i = 0; i < 256; i++)
-	{
-		pixelPro[i] = (float)(pixelCount[i]) / (float)(pixelSum);
-	}
-
-	//¾­µäostuËã·¨,µÃµ½Ç°¾°ºÍ±³¾°µÄ·Ö¸î  
-	//±éÀú»Ò¶È¼¶[0,255],¼ÆËã³ö·½²î×î´óµÄ»Ò¶ÈÖµ,Îª×î¼ÑãĞÖµ  
-	float w0, w1, u0tmp, u1tmp, u0, u1, u, deltaTmp, deltaMax = 0;
-	for (i = 0; i < 256; i++)
-	{
-		w0 = w1 = u0tmp = u1tmp = u0 = u1 = u = deltaTmp = 0;
-
-		for (j = 0; j < 256; j++)
-		{
-			if (j <= i) //±³¾°²¿·Ö  
-			{
-				//ÒÔiÎªãĞÖµ·ÖÀà£¬µÚÒ»Àà×ÜµÄ¸ÅÂÊ  
-				w0 += pixelPro[j];
-				u0tmp += j * pixelPro[j];
-			}
-			else       //Ç°¾°²¿·Ö  
-			{
-				//ÒÔiÎªãĞÖµ·ÖÀà£¬µÚ¶şÀà×ÜµÄ¸ÅÂÊ  
-				w1 += pixelPro[j];
-				u1tmp += j * pixelPro[j];
-			}
-		}
-
-		u0 = u0tmp / w0;        //µÚÒ»ÀàµÄÆ½¾ù»Ò¶È  
-		u1 = u1tmp / w1;        //µÚ¶şÀàµÄÆ½¾ù»Ò¶È  
-		u = u0tmp + u1tmp;      //Õû·ùÍ¼ÏñµÄÆ½¾ù»Ò¶È  
-								//¼ÆËãÀà¼ä·½²î  
-		deltaTmp = w0 * (u0 - u)*(u0 - u) + w1 * (u1 - u)*(u1 - u);
-		//ÕÒ³ö×î´óÀà¼ä·½²îÒÔ¼°¶ÔÓ¦µÄãĞÖµ  
-		if (deltaTmp > deltaMax)
-		{
-			deltaMax = deltaTmp;
-			threshold = i;
-		}
-	}
-	return threshold;
-}*/
-//HSV
-
-void segmentImg(int, void*) {
-
-	Mat srcImg = src.clone();
-
-	Mat HsvImg;
-	Mat ImgThreshold;
-
-	//BGR×ªHSV
-
-	cvtColor(srcImg, HsvImg, COLOR_BGR2HSV);
-
-	vector<Mat>splitHsv;
-
-	split(HsvImg, splitHsv);
-
-	equalizeHist(splitHsv[2], splitHsv[2]);
-
-	merge(splitHsv, HsvImg);
-
-
-
-	//µ÷½ÚHSV
-
-	inRange(HsvImg, Scalar(lowH, lowS, lowV), Scalar(highH, highS, highV), ImgThreshold);
-
-	Mat element = getStructuringElement(MORPH_RECT, Size(5, 5));
-
-	morphologyEx(ImgThreshold, ImgThreshold, MORPH_OPEN, element);//¿ª²Ù×÷
-
-	morphologyEx(ImgThreshold, ImgThreshold, MORPH_CLOSE, element);//±Õ²Ù×÷
-
-	namedWindow("thresholdImage", 1);
-
-	imshow("thresholdImage", ImgThreshold);
-
-	dst = ImgThreshold;
+     imshow("Contours", ContourImg);
+     DrawCircle(warped);
 
 }
 
 
+	//HSV
+	void BallCenter::segmentImg(Mat&src,Mat&dst){
+
+		Mat srcImg = src.clone();
+
+		Mat HsvImg;
+		Mat ImgThreshold;
+
+		//BGRè½¬HSV
+
+		cvtColor(srcImg, HsvImg, COLOR_BGR2HSV);
+
+		vector<Mat>splitHsv;
+
+		split(HsvImg, splitHsv);
+
+		equalizeHist(splitHsv[2], splitHsv[2]);
+
+		merge(splitHsv, HsvImg);
 
 
 
-//ÌáÈ¡ÂÖÀª¼°ÖĞĞÄµã
+		//è°ƒèŠ‚HSV
+
+		inRange(HsvImg, Scalar(lowH, lowS, lowV), Scalar(highH, highS, highV), ImgThreshold);
+
+		Mat element = getStructuringElement(MORPH_RECT, Size(5, 5));
+
+		morphologyEx(ImgThreshold, ImgThreshold, MORPH_OPEN, element);//å¼€æ“ä½œ
+
+		morphologyEx(ImgThreshold, ImgThreshold, MORPH_CLOSE, element);//é—­æ“ä½œ
+
+		namedWindow("thresholdImage", 1);
+
+		imshow("thresholdImage", ImgThreshold);
+
+		dst = ImgThreshold;
+
+	}
 
 
-void FindContours(Mat &Image, Mat &ContourImage) {
 
-	//ÌáÈ¡ÂÖÀª
+
+
+//æå–è½®å»“åŠä¸­å¿ƒç‚¹
+
+
+
+void BallCenter::FindContours(Mat &Image, Mat &ContourImage) {
+    vector<vector<Point>> contours;
+    vector<Vec4i>hierarcy;
+	//æå–è½®å»“
 
 	findContours(Image, contours, hierarcy, CV_RETR_CCOMP, CHAIN_APPROX_NONE);
 
 	//drawContours(ContourImage, contours, -1, Scalar(255, 0, 0), 2, 8, hierarcy);
 
-	if (contours.size() != 2)return;
+	if (contours.size() != 2){
+	    update=false;
+	    return;
+	}
 
-	//»æÖÆÍâ½Ó¾ØĞÎ
+	//ç»˜åˆ¶å¤–æ¥çŸ©å½¢
 
-	vector<Rect>boundRect(contours.size());
+	vector<Rect> boundRect(contours.size());
 
-	vector<vector<Point2f>>RectPoint(contours.size());
-
-
+	vector<vector<Point2f>> RectPoint(contours.size());
 
 
+	float width = 0;
+
+	float height = 0;
 
 	float CX = 0;
 
 	float CY = 0;
-
-	Point2f centerPoint;
-
 	Point2f pointFirst;
 	Point2f pointZero;
 
-	for (int i = 0; i < contours.size(); i++)
-
-	{
+	for (int i = 0; i < contours.size(); i++) {
 		float x0 = 0, y0 = 0, w0 = 0, h0 = 0;
 
-		boundRect[i] = boundingRect((Mat)contours[i]); //²éÕÒÃ¿¸öÂÖÀªµÄÍâ½Ó¾ØĞÎ
+		boundRect[i] = boundingRect((Mat) contours[i]); //æŸ¥æ‰¾æ¯ä¸ªè½®å»“çš„å¤–æ¥çŸ©å½¢
 
-		drawContours(ContourImage, contours, i, Scalar(0, 0, 255), 2, 8);  //»æÖÆÂÖÀª
+		drawContours(ContourImage, contours, i, Scalar(0, 0, 255), 2, 8);  //ç»˜åˆ¶è½®å»“
 
-		x0 = boundRect[i].x;  //»ñµÃµÚi¸öÍâ½Ó¾ØĞÎµÄ×óÉÏ½ÇµÄx×ø±ê
+		x0 = boundRect[i].x;  //è·å¾—ç¬¬iä¸ªå¤–æ¥çŸ©å½¢çš„å·¦ä¸Šè§’çš„xåæ ‡
 
-		y0 = boundRect[i].y; //»ñµÃµÚi¸öÍâ½Ó¾ØĞÎµÄ×óÉÏ½ÇµÄy×ø±ê
+		y0 = boundRect[i].y; //è·å¾—ç¬¬iä¸ªå¤–æ¥çŸ©å½¢çš„å·¦ä¸Šè§’çš„yåæ ‡
 
-		w0 = boundRect[i].width; //»ñµÃµÚi¸öÍâ½Ó¾ØĞÎµÄ¿í¶È
+		w0 = boundRect[i].width; //è·å¾—ç¬¬iä¸ªå¤–æ¥çŸ©å½¢çš„å®½åº¦
 
-		h0 = boundRect[i].height; //»ñµÃµÚi¸öÍâ½Ó¾ØĞÎµÄ¸ß¶È
+		h0 = boundRect[i].height; //è·å¾—ç¬¬iä¸ªå¤–æ¥çŸ©å½¢çš„é«˜åº¦
 
 		RectPoint[i].push_back(Point(x0, y0));
 
@@ -210,33 +164,163 @@ void FindContours(Mat &Image, Mat &ContourImage) {
 
 		RectPoint[i].push_back(Point(x0, y0 + h0));
 
-		rectangle(ContourImage, Point(x0, y0), Point(x0 + w0, y0 + h0), Scalar(0, 255, 0), 2, 8); //»æÖÆµÚi¸öÍâ½Ó¾ØĞÎ
+		rectangle(ContourImage, Point(x0, y0), Point(x0 + w0, y0 + h0), Scalar(0, 255, 0), 2, 8); //ç»˜åˆ¶ç¬¬iä¸ªå¤–æ¥çŸ©å½¢
 
 	}
 
-	float width = boundRect[0].width;
-	float height = boundRect[0].height;
 
-	for (int i = 0;i < boundRect.size() - 1;i++)
-		for (int j = i + 1;j < boundRect.size();j++) {
-			if (boundRect[i].area() < boundRect[j].area()) {
+	if (boundRect[0].area() < boundRect[1].area()) {
 
-				width = boundRect[j].width;
+		width = boundRect[1].width;
 
-				height = boundRect[j].height;
-				pointFirst = RectPoint[j][0];
-			}
-			
-			//ÅĞ¶ÏÖĞĞÄµã
-		}
+		height = boundRect[1].height;
 
-	cout << width << endl;
-	cout << height << endl;
-	cout << pointFirst;
-	//µÃµ½Ğ¡Çò×ø±ê
-	
+		pointFirst = RectPoint[1][0];
+
+		//åˆ¤æ–­ä¸­å¿ƒç‚¹
+
+		CX = (RectPoint[0][0].x + RectPoint[0][1].x) / 2.0;
+
+		CY = (RectPoint[0][0].y + RectPoint[0][3].y) / 2.0;
+		Point2f center = Point2f(CX, CY);
+		circle(ContourImage, center, 1, Scalar(255, 0, 0), 1, 8, 0);
+
+		//cout << "center:" << centerPoint;
+
+
+
+	} else {
+
+		width = boundRect[0].width;
+
+		height = boundRect[0].height;
+
+		pointFirst = RectPoint[0][0];
+
+		//åˆ¤æ–­ä¸­å¿ƒç‚¹
+
+		CX = (RectPoint[1][0].x + RectPoint[1][1].x) / 2.0;
+
+		CY = (RectPoint[1][0].y + RectPoint[1][3].y) / 2.0;
+		Point2f center = Point2f(CX, CY);
+		circle(ContourImage, center, 1, Scalar(255, 0, 0), 1, 8, 0);
+
+		//cout << "center:" << centerPoint;
+
+
+
+	}
+
+
+	centerPoint.x = (CX - pointFirst.x) * 65 / Rwidth;
+	centerPoint.y = (CY - pointFirst.y) * 65 / RHeight;
+
+}
+
+void BallCenter::contours_filter(Mat &imgThresholded, vector<vector<Point> > &contours)
+{
+    vector<Vec4i> hierarchy;
+    findContours(imgThresholded, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_NONE);//æ±‚è½®å»“ã€€
+    //åˆæ­¥ç­›é€‰ç¬¦åˆè¦æ±‚çš„çŸ©å½¢
+    Mat contour = Mat::zeros(imgThresholded.rows, imgThresholded.cols, CV_8SC3);
+    //drawContours(contour, contours, -1, Scalar(200, 200, 200), 2);
+    //imshow("contours", contour);
+
+}
+
+vector<Point2f> BallCenter::pick_point(vector<vector<Point>>&contours) {
+    vector<Point>hull;
+    vector<Point2f> point_img;
+    convexHull(contours[0], hull);
+    //cout << hull.size();
+    vector<Point> squar;
+    size_t num = hull.size();
+    if (num >= 4) {
+        float max_area;
+        for (int m = 0;m < num - 3;m++) {
+            for (int n = m + 1;n < num - 2;n++) {
+                for (int j = n + 1;j < num - 1;j++) {
+                    for (int k = j + 1;k < num;k++) {
+                        vector<Point> squar_tmp;
+                        squar_tmp.push_back(hull[m]);
+                        squar_tmp.push_back(hull[n]);
+                        squar_tmp.push_back(hull[j]);
+                        squar_tmp.push_back(hull[k]);
+                        if (m == 0 && n == 1 && j == 2 && k == 3) {
+                            max_area = fabs(contourArea(Mat(squar_tmp)));
+                            squar.clear();
+                            squar = squar_tmp;
+                        }
+                        else {
+                            float area = fabs(contourArea(Mat(squar_tmp)));
+                            if (area > max_area) {
+                                max_area = area;
+                                squar.clear();
+                                squar = squar_tmp;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    //ç»™å››ç‚¹æ’åº
+    vector<Point> squar_sort = squar;
+    for (int i = 0;i < squar_sort.size();i++) {
+        point_img.clear();
+        for (size_t num_p = 0;num_p < squar_sort.size();num_p++) {
+            // point_img.push_back(squar_sort[num_p] * (1 / minifactor));
+            point_img.push_back(squar_sort[num_p]);
+        }
+    }
+
+    vector<Point2f> point_temp = point_img;
+    point_img.clear();
+    point_img.push_back(point_temp[1]);
+    point_img.push_back(point_temp[2]);
+    point_img.push_back(point_temp[3]);
+    point_img.push_back(point_temp[0]);
+   // cout << point_img;
+    return point_img;
+}
+
+// å¯è§†åŒ–è§’ç‚¹
+void BallCenter::visual_point(Mat src, vector<Point2f> point_image)
+{
+    for (int i = 0;i < point_image.size();i++)
+    {
+        circle(src, point_image[i], 5, Scalar(255, 200, 0), 3);
+    }
+    //imshow("point", src);
+    //imwrite("point.png", src);
+}
+
+
+void BallCenter::DrawCircle(Mat&warped){
+     float Rx=Rwidth/65;
+     float Ry=RHeight/65;
+     vector<Point2f>Point;
+     Point.push_back(Point2f(12.5*Rx,12.5*Ry));
+    Point.push_back(Point2f(32.5*Rx,12.5*Ry));
+    Point.push_back(Point2f(52.5*Rx,12.5*Ry));
+    Point.push_back(Point2f(12.5*Rx,32.5*Ry));
+    Point.push_back(Point2f(32.5*Rx,32.5*Ry));
+    Point.push_back(Point2f(52.5*Rx,32.5*Ry));
+    Point.push_back(Point2f(12.5*Rx,52.5*Ry));
+    Point.push_back(Point2f(32.5*Rx,52.5*Ry));
+    Point.push_back(Point2f(52.5*Rx,52.5*Ry));
+    for(int i=0;i<Point.size();i++){
+       circle(warped,Point[i],3*Rx,Scalar(255,0,0));
+
+    }
+
+    namedWindow("Cirle",1);
+    imshow("Cirle",warped);
 
 
 
 }
+
+
+
 
---
+++ BallCenter.h
@@ -0,0 +1,35 @@
+//
+// Created by xiajiayu on 19-1-19.
+//
+
+#ifndef BALLBALANCE_BALLCENTER_H
+#define BALLBALANCE_BALLCENTER_H
+#include"Header.h"
+using namespace cv;
+using namespace std;
+class BallCenter{
+public:
+    BallCenter();
+    void process(Mat &src);
+
+private:
+    void contours_filter(Mat &imgThresholded, vector<vector<Point> > &contours);
+    vector<Point2f>pick_point(vector<vector<Point>>&contours);
+    void visual_point(Mat src, vector<Point2f> point_image);
+    void segmentImg(Mat&src,Mat &dst);
+    void FindContours(Mat&Image, Mat&ContourImage);
+    void DrawCircle(Mat &warped);
+public:
+    Point2f centerPoint=Point2f(32.5,32.5);
+    bool update=true;
+private:
+    int lowH = 155, highH = 185, lowS = 34, highS = 255, lowV = 46, highV = 255;
+    float Rwidth=396 ,RHeight = 397;
+    Point2f dst_vertices[4]={Point2f(120,427),Point2f(120,30),Point2f(516,30),Point2f(516,427)};
+
+
+};
+
+
+
+#endif //BALLBALANCE_BALLCENTER_H
